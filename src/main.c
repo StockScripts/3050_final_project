6689faf49e0a51f893d52bd2b19593ae0ba6f850
Tony Forsythe <forsythetony@gmail.com>
2016-03-14 22:14:49 -0500

    Working code with some stack testing stuff

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@
 #include "../include/vector.h"
 #include "../input_error.h"
 
-#define CORRECT_ARG_COUNT	3
-#define MAX_STRING_SIZE		512
-#define MAX_EDGE_COUNT		512
+#define VERTEX_QUEUE_INIT_SIZE 	256
+#define MAX_STRING_SIZE			512
+#define MAX_EDGE_COUNT			512
+#define SOURCE_VERTEX			1
 
 typedef enum {false, true} bool;
 
@@ -29,12 +30,30 @@ typedef struct _adjacencyList {
 	int* data;
 } AdjacencyList;
 
+typedef struct _vertexStack {
+	Vertex**	data;
+	int			size;
+	int			capacity;
+} VertexStack;
+
+void	init_vertex_stack(VertexStack* vs);
+bool	is_empty_vertex_stack(VertexStack* vs);
+void	push_vertex_stack(VertexStack* vs, Vertex* v);
+Vertex*	pop_vertex_stack(VertexStack* vs);
+void	print_info_stack(VertexStack* vs);
+
 /*
 	FUNCTION PROTOTYPES
 */
 
 
 void	get_graph_information(char* filename, Edge*** edges, Vertex*** vertices, int* vertexCount, int* edgeCount);
+void	construct_adjacency_lists(int edgeCount, Edge** edges, int vertexCount, Vertex** vertices, AdjacencyList*** adjacencyLists);
+void	bfs(int sourceVertex, int vertexCount, Vertex** vertices, AdjacencyList** adjacencyLists);
+
+
+void print_vertex(Vertex* v);
+
 
 int main(int argc, char* argv[])
 {
@@ -45,12 +64,88 @@ int main(int argc, char* argv[])
 	
 	
 	//	Pull the information from the graph input file
-	Edge** edges;
-	Vertex** vertices;
+	Edge** 				edges;
+	Vertex** 			vertices;
+	AdjacencyList**		adjLists;
+	
 	int vertexCount = -1;
 	int edgeCount = -1;
 	
 	get_graph_information(input_filepath, &edges, &vertices, &vertexCount, &edgeCount);	
+
+	construct_adjacency_lists(edgeCount, edges, vertexCount, vertices, &adjLists);
+
+	bfs( SOURCE_VERTEX, vertexCount, vertices, adjLists);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	VertexStack test_stack;
+	
+	init_vertex_stack(&test_stack);
+	
+	if (is_empty_vertex_stack(&test_stack) == true)
+	{
+		printf("\nThe stack is empty\n");
+	}
+	else
+	{
+		printf("\nThe stack apparently has some values in it...\n");
+	}
+	
+	push_vertex_stack(&test_stack, vertices[0]);
+	
+	if (is_empty_vertex_stack(&test_stack) == true)
+	{
+		printf("\nThe stack is empty\n");
+	}
+	else
+	{
+		printf("\nThe stack apparently has some values in it...\n");
+	}
+	
+	printf("\nAbout to pop some stuff\n");
+	
+	Vertex* testing = pop_vertex_stack(&test_stack);
+	
+	if (testing == NULL)
+	{
+		printf("\nThe value popped off was null...\n");
+	}
+	else
+	{
+		print_vertex(testing);
+	}
+	
+	if (is_empty_vertex_stack(&test_stack) == true)
+	{
+		printf("\nThe stack is empty\n");
+	}
+	else
+	{
+		printf("\nThe stack apparently has some values in it...\n");
+	}
+	
+	
+/*
+	AdjacencyList* temp_list = adjLists[0];
+	
+	int i = 0;
+	
+	printf("\nThe following is the adjacency list for vertex %d\n\n", temp_list->vertexValue);
+	
+	for (i = 0; i < temp_list->size; i++)
+	{
+		printf("\n%d", temp_list->data[i]);
+	}
+*/
 }
 
 
@@ -126,8 +221,128 @@ void get_graph_information(char* filename, Edge*** edges, Vertex*** vertices, in
 	}
 	
 	if (did_find_edge == true)
-		*edgeCount = edge_index;
+		*edgeCount = edge_index;	
+}
+
+void construct_adjacency_lists(int edgeCount, Edge** edges, int vertexCount, Vertex** vertices, AdjacencyList*** adjacencyLists)
+{
+	//	Allocate memory for all the adjacency lists
+	
+	*adjacencyLists = malloc(vertexCount * sizeof(AdjacencyList*));
+	
+	int i = 0;
+	
+	for (i = 0; i < vertexCount; i++)
+	{
+		(*adjacencyLists)[i] = malloc( sizeof(AdjacencyList) );
+		(*adjacencyLists)[i]->vertexValue = i + 1;
+	}
 	
 	
+	//	Use the edge information to construct adjacency lists for each node
+	
+	for (i = 0; i < vertexCount; i++)
+	{
+		int vertex_value = vertices[i]->value;
 		
+		struct vector temp_vector;
+		
+		init_vector(&temp_vector);
+		
+		
+		int j = 0;
+		
+		for (j = 0; j < edgeCount; j++)
+		{
+			if (vertex_value == edges[j]->start)
+				insert_element_vector(&temp_vector, edges[j]->end);
+				
+			if (vertex_value == edges[j]->end)
+				insert_element_vector(&temp_vector, edges[j]->start);
+		}
+		
+		int vector_size = temp_vector.size;
+		int* static_vector_array;
+		
+		convert_to_static_int_array(&temp_vector, &static_vector_array);
+		
+		(*adjacencyLists)[i]->vertexValue = vertex_value;
+		(*adjacencyLists)[i]->size = vector_size;
+		(*adjacencyLists)[i]->data = static_vector_array;
+		
+		free_vector(&temp_vector);
+	}
+}
+void bfs(int sourceVertex, int vertexCount, Vertex** vertices, AdjacencyList** adjacencyLists)
+{
+	
+}
+
+
+
+
+
+
+
+
+
+/*
+	VERTEX STACK FUNCTIONS
+*/
+void init_vertex_stack(VertexStack* vs)
+{
+	vs->data = malloc( VERTEX_QUEUE_INIT_SIZE * sizeof(Vertex*));
+	vs->size = 0;
+	vs->capacity = VERTEX_QUEUE_INIT_SIZE; 
+}
+bool is_empty_vertex_stack(VertexStack* vs)
+{
+	if (vs->size == 0)
+		return true;
+	
+	return false;
+}
+void push_vertex_stack(VertexStack* vs, Vertex* v)
+{
+	if (vs->capacity == vs->size) {
+		vs->data = realloc(vs->data, sizeof(Vertex*) * vs->capacity * 2);
+		vs->capacity *= 2;
+	}
+	print_vertex(v);
+	vs->data[vs->size] = v;
+	vs->size += 1;
+}
+Vertex*	pop_vertex_stack(VertexStack* vs)
+{
+	if (vs->size == 0)
+		return NULL;
+		
+	vs->size -= 1;
+	
+	return vs->data[vs->size];
+}
+void print_info_stack(VertexStack* vs)
+{
+	if ( vs == NULL )
+	{
+		printf("\nThere's really nothing to print because the stack you gave me was null...\n")
+	}
+	else
+	{
+		printf("\n\nIs Empty:\t%s\nSize:\t%d\nCapacity:\t%d\n\n", ((vs->size == 0) ? "Yes" : "No"), vs->size, vs->capacity);
+	}
+}
+
+/*
+	UTILITY FUNCTIONS
+*/
+void print_vertex(Vertex* v)
+{
+	if (v == NULL)
+	{
+		printf("\nThe vertex was NULL. Quitting now...\n");
+		exit(1);
+	}
+	
+	printf("\n{ Value : %d, Distance : %d }", v->value, v->distanceFromSource);
 }
